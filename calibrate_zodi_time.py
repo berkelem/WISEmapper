200334b9b685360bccecaac28885a030953d3afd
berkelem <42berkeley@cua.edu>
2020-05-06 00:06:16 +0200

    10 iterations

--- calibrate_zodi_time.py
+++ calibrate_zodi_time.py
@@ -50,7 +50,7 @@ class Coadder:
 
     def run(self):
         num_orbits = 100
-        iterations = 4
+        iterations = 10
         self.gains = np.zeros(num_orbits)
         self.offsets = np.zeros_like(self.gains)
         self.orbit_sizes = np.zeros_like(self.gains)
