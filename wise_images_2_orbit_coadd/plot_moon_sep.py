cc24c59136f924097b88937ac793b3371ab91224
berkelem <42berkeley@cua.edu>
2020-10-13 23:37:55 +0200

    extract min/max moon seps per scan

--- wise_images_2_orbit_coadd/plot_moon_sep.py
+++ wise_images_2_orbit_coadd/plot_moon_sep.py
@@ -6,4 +6,17 @@ if __name__ == "__main__":
     meta_reader.load_metadata(filename)
 
     moon_sep_by_scan = meta_reader.dataframe[["moon_sep", "scan_id"]]
-    print(moon_sep_by_scan)
+
+    scan_groups = moon_sep_by_scan.groupby(moon_sep_by_scan["scan_id"])
+
+    min_seps = []
+    max_seps = []
+    for name, group in scan_groups:
+        minval = min(group["moon_sep"])
+        maxval = max(group("moon_sep"])
+        min_seps.append(minval)
+        max_seps.append(maxval)
+
+
+    print(min_seps)
+    print(max_seps)
