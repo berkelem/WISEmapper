d5a613ef2fc6212924263ff4ed6de403d746a362
berkelem <42berkeley@cua.edu>
2020-10-13 23:27:01 +0200

    add moon sep plot script

---
+++ wise_images_2_orbit_coadd/plot_moon_sep.py
@@ -0,0 +1,9 @@
+from wise_images_2_orbit_coadd.wise_file_selection import MetaDataReader
+
+if __name__ == "__main__":
+    filename = "all_band3.tbl"
+    meta_reader = MetaDataReader()
+    meta_reader.load_metadata(filename)
+
+    moon_sep = meta_reader.dataframe["moon_sep"]
+    print(moon_sep)
