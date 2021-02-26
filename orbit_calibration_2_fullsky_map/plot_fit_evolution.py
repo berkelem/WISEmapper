0c967cd2cc58aa5b8321e6286dcd6ea487570b1e
berkelem <42berkeley@cua.edu>
2021-02-26 12:38:49 +0100

    fix

--- orbit_calibration_2_fullsky_map/plot_fit_evolution.py
+++ orbit_calibration_2_fullsky_map/plot_fit_evolution.py
@@ -2,6 +2,6 @@ from orbit_calibration_2_fullsky_map.spline_fit_calibration import SplineFitter
 
 
 if __name__ == "__main__":
-    sf = SplineFitter(25)
+    sf = SplineFitter(24)
     for i in range(0, 212, 10):
         sf._plot_fit_evolution(i)
