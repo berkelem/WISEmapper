2a9d65795e56feefafb5fb33f52eae5b30aaa586
berkelem <42berkeley@cua.edu>
2021-03-04 12:40:57 +0100

    fix

--- orbit_calibration_2_fullsky_map/plot_gain_offset_evolution.py
+++ orbit_calibration_2_fullsky_map/plot_gain_offset_evolution.py
@@ -2,6 +2,6 @@ from orbit_calibration_2_fullsky_map.spline_fit_calibration import SplineFitter
 import sys
 
 if __name__ == "__main__":
-    for i in range(sys.argv[1], sys.argv[2], sys.argv[3]):
+    for i in range(int(sys.argv[1]), int(sys.argv[2]), int(sys.argv[3])):
         sf = SplineFitter(i)
         sf._plot_all_fitvals()
