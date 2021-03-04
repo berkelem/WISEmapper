9a35cbb2fbf393b2a7c39049519dc98d1c7b3559
berkelem <42berkeley@cua.edu>
2021-03-04 12:37:50 +0100

    add gain/offset evolution plot

--- orbit_calibration_2_fullsky_map/plot_gain_offset_evolution.py
+++ orbit_calibration_2_fullsky_map/plot_gain_offset_evolution.py
@@ -2,6 +2,6 @@ from orbit_calibration_2_fullsky_map.spline_fit_calibration import SplineFitter
 import sys
 
 if __name__ == "__main__":
-    sf = SplineFitter(sys.argv[1])
-    for i in range(0, 212, 10):
-        sf._plot_fit_evolution(i)
+    for i in range(sys.argv[1], sys.argv[2], sys.argv[3]):
+        sf = SplineFitter(i)
+        sf._plot_all_fitvals()
