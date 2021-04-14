a67631ecf3fa39e79cf92a22b860b1ef1580e725
berkelem <42berkeley@cua.edu>
2021-04-14 13:19:15 +0200

    fixes

--- orbit_calibration_2_fullsky_map/plot_orbit.py
+++ orbit_calibration_2_fullsky_map/plot_orbit.py
@@ -3,18 +3,30 @@ from orbit_calibration_2_fullsky_map.spline_fit_calibration import SplineFitter
 import os
 
 
-
 if __name__ == "__main__":
-    output_path = os.getcwd()
+
+    # Declare paths to important input files/directories
+    moon_stripe_file = "/home/users/mberkeley/wisemapper/data/masks/moon_stripe_mask_G_thick3.fits"  # Input file, already exists
     orbit_file_path = "/home/users/mberkeley/wisemapper/data/output_maps/w3_days_mooncut/csv_files/"  # Input files, path already exists
     zodi_file_path = "/home/users/jguerraa/AME/cal_files/W3_days_mooncut/"  # Input files, path already exists
-    coadd_map = Coadder(3, None, None, orbit_file_path, zodi_file_path, ".")
 
-    setattr(Orbit, "orbit_file_path", orbit_file_path)
-    setattr(Orbit, "zodi_file_path", zodi_file_path)
+    # Declare outputs
+    output_path = os.getcwd()  # Specify where to store output files from calibration
+    # Specify desired name (and path) of output full-sky map
+    fsm_map_file = os.path.join(output_path, "fullskymap_band3_masked.fits")
+    iterations = 25
+
+    # Initialize Coadder object for managing calibration
+    coadd_map = Coadder(3, moon_stripe_file, fsm_map_file, orbit_file_path, zodi_file_path, output_path)
     orbit = Orbit(141, 3, None, 256)
     orbit.load_orbit_data()
     orbit.load_zodi_orbit_data()
-    sf = SplineFitter(iter_num=24, path_to_fitvals=output_path)
+
+    # Fit a spline through the converged fit values for gains and offsets
+    sf = SplineFitter(iter_num=iterations-1, path_to_fitvals=output_path)
+    sf.fit_spline()
+
+    # Load the spline back into the Coadder and do a final calibration
     coadd_map.load_splines(sf.gain_spline_file, sf.offset_spline_file)
+
     orbit.plot_fit(output_path)
