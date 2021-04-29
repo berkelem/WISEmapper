4bb4d67c381d10bc61c96c3101fd44a6a0b72b94
berkelem <42berkeley@cua.edu>
2021-04-29 18:28:52 +0200

    remove jan and aug

--- orbit_calibration_2_fullsky_map/rotate_map.py
+++ orbit_calibration_2_fullsky_map/rotate_map.py
@@ -4,6 +4,6 @@ from wise_images_2_orbit_coadd.file_handler import WISEMap
 filename = sys.argv[1]
 wisemap = WISEMap(filename, 3)
 wisemap.read_data()
-wisemap.filename = filename.replace(".fits", "_E.fits")
-wisemap.rotate_map("G", "E")
-wisemap.save_map("E")
+wisemap.filename = filename.replace(".fits", "_C.fits")
+wisemap.rotate_map("G", "C")
+wisemap.save_map("C")
