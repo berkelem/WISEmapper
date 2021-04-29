b08b1f46588dd67840253e0667078c0d7fa35cbc
berkelem <42berkeley@cua.edu>
2021-04-29 14:49:01 +0200

    add rotate map script

---
+++ orbit_calibration_2_fullsky_map/rotate_map.py
@@ -0,0 +1,8 @@
+import sys
+from wise_images_2_orbit_coadd.file_handler import WISEMap
+
+filename = sys.argv[1]
+wisemap = WISEMap(filename, 3)
+wisemap.filename = filename.replace(".fits", "_E.fits")
+wisemap.rotate_map("G", "E")
+wisemap.save_map("E")
