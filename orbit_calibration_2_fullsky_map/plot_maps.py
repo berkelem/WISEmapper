4c3ed29f8c6dc39715f643a43e1e66c3b6e023da
berkelem <42berkeley@cua.edu>
2021-05-06 13:36:35 +0200

    jan feb

--- orbit_calibration_2_fullsky_map/plot_maps.py
+++ orbit_calibration_2_fullsky_map/plot_maps.py
@@ -5,5 +5,5 @@ filename = sys.argv[1]
 title = sys.argv[2]
 fsm = WISEMap(filename, 3)
 fsm.read_data()
-fsm.plot_mollweide(title, min=0, max=2)
+fsm.plot_mollweide(title, min=0, max=2, rot=[-90, -30])
 
