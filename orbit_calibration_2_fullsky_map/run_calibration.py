f4f04a50515856648f47ec68a4f48a35c83d9002
berkelem <42berkeley@cua.edu>
2021-05-12 17:28:43 +0200

    calibrate band 4

--- orbit_calibration_2_fullsky_map/run_calibration.py
+++ orbit_calibration_2_fullsky_map/run_calibration.py
@@ -6,23 +6,23 @@ if __name__ == "__main__":
 
     # Declare paths to important input files/directories
     moon_stripe_file = "/home/users/mberkeley/wisemapper/data/masks/moon_stripe_mask_G_thick3.fits"#south_mask.fits"#moon_stripe_mask_G_thick3.fits"  # Input file, already exists
-    orbit_file_path = "/home/users/mberkeley/wisemapper/data/output_maps/w3_days_mooncut/csv_files/"  # Input files, path already exists
-    zodi_file_path = "/home/users/jguerraa/AME/cal_files/W3_days_mooncut/"  # Input files, path already exists
+    orbit_file_path = "/home/users/mberkeley/wisemapper/data/output_maps/w4/csv_files/"#w3_days_mooncut/csv_files/"  # Input files, path already exists
+    zodi_file_path = "/home/users/jguerraa/AME/cal_files/W4/"#W3_days_mooncut/"  # Input files, path already exists
 
     # Declare outputs
     output_path = os.getcwd()  # Specify where to store output files from calibration
     # Specify desired name (and path) of output full-sky map
-    fsm_map_file = os.path.join(output_path, "fullskymap_band3_masked.fits")
+    fsm_map_file = os.path.join(output_path, "fullskymap_band4_masked.fits")
     iterations = 15
 
     # Initialize Coadder object for managing calibration
     coadd_map = Coadder(3, moon_stripe_file, fsm_map_file, orbit_file_path, zodi_file_path, output_path)
-    coadd_map.load_orbits(month=["Jul", "Aug"])
-    # coadd_map.run_iterative_fit(iterations=iterations)
+    coadd_map.load_orbits(month=["Feb", "Aug"])
+    coadd_map.run_iterative_fit(iterations=iterations)
 
     # Fit a spline through the converged fit values for gains and offsets
     sf = SplineFitter(iter_num=iterations-1, path_to_fitvals=output_path)
-    # sf.fit_spline()
+    sf.fit_spline()
 
     # Load the spline back into the Coadder and do a final calibration
     coadd_map.load_splines(sf.gain_spline_file, sf.offset_spline_file)
