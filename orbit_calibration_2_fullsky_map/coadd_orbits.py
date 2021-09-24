af64507595416a7a20a2111922bc8adafdad2315
berkelem <42berkeley@cua.edu>
2021-09-24 23:53:48 +0200

    fix deg/rad error

--- orbit_calibration_2_fullsky_map/coadd_orbits.py
+++ orbit_calibration_2_fullsky_map/coadd_orbits.py
@@ -542,8 +542,8 @@ class IterativeFitter:
         offsets = []
         for bin in bins:
             start_phi_deg, end_phi_deg = bin
-            start_phi = start_phi_deg*np.pi/180.0
-            end_phi = end_phi_deg*np.pi/180.0
+            start_phi = start_phi_deg
+            end_phi = end_phi_deg
             segment_mask = (self.phi >= start_phi) & (self.phi < end_phi)
             segment_data = orbit_data[segment_mask]
             segment_uncs = orbit_uncs[segment_mask]
