dc399838e73afb57cb5268900ca12bcac0da1c4c
berkelem <42berkeley@cua.edu>
2021-09-29 00:56:55 +0200

    try again lonlat

--- orbit_calibration_2_fullsky_map/coadd_orbits.py
+++ orbit_calibration_2_fullsky_map/coadd_orbits.py
@@ -373,7 +373,7 @@ class Orbit(BaseMapper):
 
     def plot_fit(self, output_path=os.getcwd(), iteration=None, label=None):
         """Plot calibrated data along with the zodiacal light template with galactic latitude on the x-axis"""
-        theta, phi = hp.pix2ang(self._nside, self.pixel_inds_clean_masked)
+        theta, phi = hp.pix2ang(self._nside, self.pixel_inds_clean_masked, lonlat=True)
         plt.plot(phi, self._cal_data_clean_masked, "r.", ms=0.5, alpha=0.5, label="Calibrated data")
         plt.plot(phi, self._zodi_data_clean_masked, "b.", ms=0.5, alpha=0.5, label="Zodi model")
         plt.legend()
@@ -389,7 +389,7 @@ class Orbit(BaseMapper):
         plt.close()
 
     def plot_diff(self, output_path=os.getcwd()):
-        theta, phi = hp.pix2ang(self._nside, self.pixel_inds_clean_masked)
+        theta, phi = hp.pix2ang(self._nside, self.pixel_inds_clean_masked, lonlat=True)
         plt.plot(phi, self._cal_data_clean_masked-self._zodi_data_clean_masked, "r.", ms=0.5)
         plt.title("Orbit {}".format(self.orbit_num))
         plt.xlabel("Latitude (degrees)")
