11b1dba75eb7d548aa6451dde3c8f49192a35d6b
berkelem <42berkeley@cua.edu>
2021-11-12 11:59:11 +0100

    feb stripe2 seek

--- orbit_calibration_2_fullsky_map/coadd_orbits.py
+++ orbit_calibration_2_fullsky_map/coadd_orbits.py
@@ -774,8 +774,8 @@ class Coadder:
         self.month_timestamps = OrderedDict(
             [
                 ("Jan", 55197),
-                ("Feb", 55245),#55228),
-                ("Mar", 55253),#55256),
+                ("Feb", 55248),#55228),
+                ("Mar", 55254),#55256),
                 ("Apr", 55287),
                 ("May", 55317),
                 ("Jun", 55348),
