075eda78463eebced9ebacbcddbe12f1f883a241
berkelem <42berkeley@cua.edu>
2021-11-12 14:31:21 +0100

    mar stripe2 apply

--- orbit_calibration_2_fullsky_map/coadd_orbits.py
+++ orbit_calibration_2_fullsky_map/coadd_orbits.py
@@ -775,16 +775,16 @@ class Coadder:
             [
                 ("Jan", 55197),
                 ("Feb", 55228),
-                ("Mar", 55277),#55256),
-                ("Apr", 55283),#55287),
+                ("Mar", 55256),
+                ("Apr", 55287),
                 ("May", 55317),
                 ("Jun", 55348),
                 ("Jul", 55378),
                 ("Aug", 55409),
             ]
         )
-        self.mask_orbit_timestamps = [(55217, 55225), (55229, 55236), (55247, 55254), (55258, 55262), (55288, 55294), (55316, 55325), (55346, 55354), (55376, 55384), (55405, 56000)]
-        self.mask_rules_stripes = ["phi_pos", "phi_neg", "phi_pos", "phi_neg", "phi_neg", "phi_neg", "phi_neg", "phi_pos", "phi_pos"]
+        self.mask_orbit_timestamps = [(55217, 55225), (55229, 55236), (55247, 55254), (55258, 55262), (55277, 55283), (55288, 55294), (55316, 55325), (55346, 55354), (55376, 55384), (55405, 56000)]
+        self.mask_rules_stripes = ["phi_pos", "phi_neg", "phi_pos", "phi_neg", "phi_pos", "phi_neg", "phi_neg", "phi_neg", "phi_pos", "phi_pos"]
 
         self.all_orbits = []
 
