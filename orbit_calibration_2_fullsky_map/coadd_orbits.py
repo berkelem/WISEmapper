1e77dfb72df3d4f8c8282e68a0bb9b48a6944d37
berkelem <42berkeley@cua.edu>
2022-01-25 13:46:12 +0100

    don't subtract diff

--- orbit_calibration_2_fullsky_map/coadd_orbits.py
+++ orbit_calibration_2_fullsky_map/coadd_orbits.py
@@ -289,8 +289,8 @@ class Orbit(BaseMapper):
         )
         self.calc_rsq()
 
-        diff_spline = self.plot_diff()
-        self._cal_data_clean_masked -= diff_spline
+        # diff_spline = self.plot_diff()
+        # self._cal_data_clean_masked -= diff_spline
         self.zs_data_clean_masked = (
                 self._cal_data_clean_masked - self._zodi_data_clean_masked
         )
