de78e6a628c74ff00971e13f59cab1ad3bf2995c
berkelem <42berkeley@cua.edu>
2022-02-16 12:07:17 +0100

    test feb

--- orbit_calibration_2_fullsky_map/run_calibration.py
+++ orbit_calibration_2_fullsky_map/run_calibration.py
@@ -17,7 +17,7 @@ if __name__ == "__main__":
 
     # Initialize Coadder object for managing calibration
     coadd_map = Coadder(3, moon_stripe_file, fsm_map_file, orbit_file_path, zodi_file_path, output_path)
-    coadd_map.load_orbits(month="Jul")#["Feb", "Mar", "Apr", "May", "Jun", "Jul"])
+    coadd_map.load_orbits(month="Feb")#["Feb", "Mar", "Apr", "May", "Jun", "Jul"])
     # coadd_map.load_fitvals(19)
     coadd_map.run_iterative_fit(iterations=iterations)
 
