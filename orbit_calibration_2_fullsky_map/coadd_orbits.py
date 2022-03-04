b4e987bff63e7d3ea2e8706d6144b933c1eba488
berkelem <42berkeley@cua.edu>
2022-03-05 00:16:57 +0100

    create map of diffs

--- orbit_calibration_2_fullsky_map/coadd_orbits.py
+++ orbit_calibration_2_fullsky_map/coadd_orbits.py
@@ -299,7 +299,8 @@ class Orbit(BaseMapper):
         # self.zs_data_clean_masked = (
         #         self._cal_data_clean_masked - self._zodi_data_clean_masked
         # )
-        self.zs_data_clean_masked = diff_data
+        self.zs_data_clean_masked = np.zeros_like(self._cal_data_clean_masked)
+        self.zs_data_clean_masked[self.galaxy_mask] = diff_data
 
         return
 
