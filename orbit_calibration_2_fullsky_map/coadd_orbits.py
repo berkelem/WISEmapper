1cae3b206f49b19f0c105612483b89ee62d7986e
berkelem <42berkeley@cua.edu>
2022-04-19 16:10:01 +0100

    fix iterative data array lengths

--- orbit_calibration_2_fullsky_map/coadd_orbits.py
+++ orbit_calibration_2_fullsky_map/coadd_orbits.py
@@ -637,9 +637,10 @@ class IterativeFitter:
         data_to_fit = self.raw_data
         uncs_to_fit = self.raw_uncs
         zodi_to_fit = self.zodi_data
-        gain = offset = shift = 0.0
+        gain = offset = 0.0
         if len(data_to_fit) > 0:
             while i < n:
+                shift = 0
                 # gain = 75.0
                 # offset = self._fit_offset(data_to_fit, self.zodi_data, uncs_to_fit, gain)
                 gain, offset, shift = self._fit_to_zodi(
@@ -647,7 +648,7 @@ class IterativeFitter:
                 )
                 # offset_spline = self.fit_offset_spline(data_to_fit, gain, offset)
                 # segmented_offsets = self._segmented_fit(data_to_fit, uncs_to_fit, gain)
-                data_to_fit, zodi_to_fit = self._adjust_data(gain, offset, int(shift), data_to_fit, zodi_to_fit)
+                data_to_fit, uncs_to_fit, zodi_to_fit = self._adjust_data(gain, offset, int(shift), data_to_fit, uncs_to_fit, zodi_to_fit)
                 i += 1
         else:
             gain = offset = shift = 0.0
@@ -717,7 +718,7 @@ class IterativeFitter:
         )
         return chi_sq
 
-    def _adjust_data(self, gain, offset, shift, data, zodi):
+    def _adjust_data(self, gain, offset, shift, data, uncs, zodi):
         """
         Subtract residual from original data
 
@@ -735,7 +736,8 @@ class IterativeFitter:
         new_zodi = zodi[shift:]
         residual = ((data[:len(data) - shift] - offset) / gain) - new_zodi
         new_data = data[:len(data) - shift] - gain * residual
-        return new_data, new_zodi
+        new_uncs = uncs[:len(uncs) - shift]
+        return new_data, new_uncs, new_zodi
 
     def _segmented_fit(self, orbit_data, orbit_uncs, gain):
         bins = [(-180, -165), (-165, -150), (-150, -135), (-135, -120), (-120, -105), (-105, -90), (-90, -75),
